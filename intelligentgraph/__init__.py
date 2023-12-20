6ee5985a6b3461d03b4fa9f3c9786b6d52bb5ff9
Lawrence <peter.lawrence@inova8.com>
2023-12-20 20:00:50 +0000

    Update __init__.py

--- intelligentgraph/__init__.py
+++ intelligentgraph/__init__.py
@@ -1,47 +1,5 @@
 __all__ = ["graph","script"]
 
-from rdflib.namespace import Namespace,DefinedNamespace
-from rdflib import Graph, ConjunctiveGraph, Dataset, Literal, URIRef, term
-from rdflib.paths import Path
-from rdflib.term import Node
-from typing import Generator ,Tuple,Optional,Union ,List
-
 from intelligentgraph.graph import IntelligentConjunctiveGraph, IntelligentDataset, IntelligentGraph
 from intelligentgraph.script import SCRIPT
 
-_SubjectType = Node
-_PredicateType = Node
-_ObjectType = Node
-_TripleType = Tuple["_SubjectType", "_PredicateType", "_ObjectType"]
-_ContextType = Graph
-_TripleSelectorType = Tuple[
-    Optional["_SubjectType"],
-    Optional[Union["Path", "_PredicateType"]],
-    Optional["_ObjectType"],
-]
-_TriplePathType = Tuple["_SubjectType", Path, "_ObjectType"]
-_TripleOrTriplePathType= Union["_TripleType", "_TriplePathType"]
-
-_QuadSelectorType = Tuple[
-    Optional["_SubjectType"],
-    Optional[Union["Path", "_PredicateType"]],
-    Optional["_ObjectType"],
-    Optional["_ContextType"],
-]
-_TripleOrQuadSelectorType = Union["_TripleSelectorType", "_QuadSelectorType"]
-_TriplePathPatternType = Tuple[Optional["_SubjectType"], Path, Optional["_ObjectType"]]
-_QuadPathPatternType = Tuple[
-    Optional["_SubjectType"],
-    Path,
-    Optional["_ObjectType"],
-    Optional["_ContextType"],
-]
-_TripleOrQuadPatternType = Union["_TriplePatternType", "_QuadPatternType"]
-_OptionalQuadType = Tuple[
-    "_SubjectType", "_PredicateType", "_ObjectType", Optional["_ContextType"]
-]
-_TriplePatternType = Tuple[
-    Optional["_SubjectType"], Optional["_PredicateType"], Optional["_ObjectType"]
-]
-
-#_ContextType = Graph
