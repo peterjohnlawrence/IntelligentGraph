4ede90ed3ab97ca9d936dcb48e54f303fe828980
Lawrence <peter.lawrence@inova8.com>
2023-12-20 17:30:32 +0000

    Update script.py

--- intelligentgraph/script.py
+++ intelligentgraph/script.py
@@ -1,3 +1,43 @@
+from rdflib.namespace import Namespace,DefinedNamespace
+from rdflib import Graph, ConjunctiveGraph, Dataset, Literal, URIRef, term
+from rdflib.paths import Path
+from rdflib.term import Node
+from typing import Generator ,Tuple,Optional,Union ,List
+
+_SubjectType = Node
+_PredicateType = Node
+_ObjectType = Node
+_TripleType = Tuple["_SubjectType", "_PredicateType", "_ObjectType"]
+_TripleSelectorType = Tuple[
+    Optional["_SubjectType"],
+    Optional[Union["Path", "_PredicateType"]],
+    Optional["_ObjectType"],
+]
+_TriplePathType = Tuple["_SubjectType", Path, "_ObjectType"]
+_TripleOrTriplePathType= Union["_TripleType", "_TriplePathType"]
+
+_QuadSelectorType = Tuple[
+    Optional["_SubjectType"],
+    Optional[Union["Path", "_PredicateType"]],
+    Optional["_ObjectType"],
+    Optional["_ContextType"],
+]
+_TripleOrQuadSelectorType = Union["_TripleSelectorType", "_QuadSelectorType"]
+_TriplePathPatternType = Tuple[Optional["_SubjectType"], Path, Optional["_ObjectType"]]
+_QuadPathPatternType = Tuple[
+    Optional["_SubjectType"],
+    Path,
+    Optional["_ObjectType"],
+    Optional["_ContextType"],
+]
+_TripleOrQuadPatternType = Union["_TriplePatternType", "_QuadPatternType"]
+_OptionalQuadType = Tuple[
+    "_SubjectType", "_PredicateType", "_ObjectType", Optional["_ContextType"]
+]
+_TriplePatternType = Tuple[
+    Optional["_SubjectType"], Optional["_PredicateType"], Optional["_ObjectType"]
+]
+
 class SCRIPT(DefinedNamespace):
   """
   SCRIPT vocabulary
